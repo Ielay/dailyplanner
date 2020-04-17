13b9df8bc06340950639312712074a25336a7cff
ielay <ielay@yandex.ru>
2020-04-18 01:30:55 +0300

    + Created configuration server and based application for Daily Planner project + Added simple test config into configuration server repository for dailyplanner module + Added bootstrap config for dailyplanner module

---
+++ src/main/java/com/dailyplanner/Application.java
@@ -0,0 +1,16 @@
+package com.dailyplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lelay
+ * @since 22.03.20
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
