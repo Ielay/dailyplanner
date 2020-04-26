761f67f8c536e3ed4f416daf152735bb3a06c7e8
ielay <ielay@yandex.ru>
2020-04-26 19:32:29 +0300

    + Have fixed changelog path in properties file + Added data source parameters into configserver's dailyplanner.yml + Fixed relativity of changelog paths + Created DataSource and Liquibase beans and migrate the first version of database

--- src/main/java/com/dailyplanner/task/configuration/TaskConfiguration.java
+++ src/main/java/com/dailyplanner/task/configuration/TaskConfiguration.java
@@ -1,10 +1,14 @@
 package com.dailyplanner.task.configuration;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * @author lelay
@@ -18,7 +22,27 @@ public class TaskConfiguration {
     private Environment env;
 
     @Bean
-    public String test() {
-        return "aaaaaaaaaa";
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+
+        String prop = env.getProperty("liquibase.changeLogFile");
+        System.out.println(prop.equals("classpath:liquibase/changelog-master.xml"));
+
+        liquibase.setChangeLog("classpath:liquibase/changelog-master.xml");
+        liquibase.setDataSource(dataSource);
+
+        return liquibase;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+
+        ds.setUsername(env.getProperty("ds.username"));
+        ds.setPassword(env.getProperty("ds.password"));
+        ds.setDriverClassName(env.getProperty("ds.driver"));
+        ds.setUrl(env.getProperty("ds.url"));
+
+        return ds;
     }
 }
