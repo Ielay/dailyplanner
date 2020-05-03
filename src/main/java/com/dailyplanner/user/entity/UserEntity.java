d3e1d122e68823400feea930bb2a1f2403855e36
ielay <ielay@yandex.ru>
2020-05-03 23:07:55 +0300

    + UserEntity has been added

---
+++ src/main/java/com/dailyplanner/user/entity/UserEntity.java
@@ -0,0 +1,56 @@
+package com.dailyplanner.user.entity;
+
+import javax.persistence.*;
+
+/**
+ * @author petrov
+ */
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
